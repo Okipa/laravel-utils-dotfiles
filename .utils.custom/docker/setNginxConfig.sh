5d7b66c8b513392c3ea51b0d32e7a258c81cb1c5
Arthur LORENT <arthur@acid.fr>
2018-04-03 16:43:39 +0200

    updated custom nginx config example

--- .utils.custom/docker/setNginxConfig.sh
+++ .utils.custom/docker/setNginxConfig.sh
@@ -3,6 +3,6 @@
 # example
 
 ## domain
-cp -f ${LARADOCK_DIRECTORY_PATH}nginx/sites/laravel.conf.example ${LARADOCK_DIRECTORY_PATH}nginx/sites/${NGINX_DOMAIN}.conf
-sed -i 's#server_name laravel.test;#server_name '${NGINX_DOMAIN}';#g' ${LARADOCK_DIRECTORY_PATH}nginx/sites/${NGINX_DOMAIN}.conf
-sed -i 's#root /var/www/laravel/public;#root /var/www/public;#g' ${LARADOCK_DIRECTORY_PATH}nginx/sites/${NGINX_DOMAIN}.conf
+#cp -f ${LARADOCK_DIRECTORY_PATH}nginx/sites/laravel.conf.example ${LARADOCK_DIRECTORY_PATH}nginx/sites/${NGINX_DOMAIN}.conf
+#sed -i 's#server_name laravel.test;#server_name '${NGINX_DOMAIN}';#g' ${LARADOCK_DIRECTORY_PATH}nginx/sites/${NGINX_DOMAIN}.conf
+#sed -i 's#root /var/www/laravel/public;#root /var/www/public;#g' ${LARADOCK_DIRECTORY_PATH}nginx/sites/${NGINX_DOMAIN}.conf
