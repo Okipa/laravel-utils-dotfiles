ba0e5d30a3b64e98ef42f6ce69e131a3c69df2f6
Arthur LORENT <arthur@acid.fr>
2018-04-03 15:16:21 +0200

    fixed example files

--- .utils.custom/project/install.sh
+++ .utils.custom/project/install.sh
@@ -3,7 +3,7 @@
 # example
 
 #echo -e "${gray}=================================================${reset}\n"
-
+#
 ## bower install / update
 #echo "${purple}▶${reset} Installing / updating bower dependencies ..."
 #if [ -d bower_components ]; then
@@ -17,25 +17,25 @@
 #    ./node_modules/bower/bin/bower install
 #    echo -e "${green}✔${reset} Bower dependencies installed\n"
 #fi
-
+#
 #echo -e "${gray}=================================================${reset}\n"
-
+#
 ## we prepare the storage folders
 #echo "${purple}▶${reset} Preparing storage folders ..."
 #echo "${purple}→ php artisan storage:prepare${reset}"
 #php artisan storage:prepare
 #echo "${green}✔${reset} Storage folders prepared"
-
+#
 #echo -e "${gray}=================================================${reset}\n"
-
+#
 ## we prepare the symbolic links
 #echo "${purple}▶${reset} Preparing app symlinks ..."
 #echo "${purple}→ php artisan symlinks:prepare${reset}"
 #php artisan symlinks:prepare
 #echo "${green}✔${reset} App symlinks created"
-
+#
 #echo -e "${gray}=================================================${reset}\n"
-
+#
 ## project optimizations
 #echo "${purple}▶${reset} Executing project optimizations ..."
 #echo "${purple}→ php artisan project:optimize${reset}"
