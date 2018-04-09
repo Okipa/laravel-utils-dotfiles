59cc447d992f94571fb4a33d0837f30499e23a5a
Arthur LORENT <arthur@acid.fr>
2018-04-09 10:50:41 +0200

    added --force to submodules update

--- git/submodules/update.sh
+++ git/submodules/update.sh
@@ -16,5 +16,5 @@ echo "${purple}▶${reset} Updating project git submodules ..."
 echo "${purple}→ git submodule sync --recursive${reset}"
 git submodule sync --recursive
 echo "${purple}→ git submodule update --init --recursive --remote --force${arguments}${reset}"
-git submodule update --init --recursive --remote ${arguments}
+git submodule update --init --recursive --remote --force ${arguments}
 echo -e "${green}✔${reset} The git submodules have been updated.\n"
