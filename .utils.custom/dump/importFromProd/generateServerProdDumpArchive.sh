fc7341b2dc43878a08743b07ed683efa351683ae
Artu1000 <arthur.lorent@gmail.com>
2018-04-09 23:16:16 +0200

    updated dump scripts

--- .utils.custom/dump/importFromProd/generateServerProdDumpArchive.sh
+++ .utils.custom/dump/importFromProd/generateServerProdDumpArchive.sh
@@ -12,8 +12,16 @@ source ${dumpImportFromProdGenerateServerProdDumpArchiveScriptDirectory}/setRequ
 source $(realpath ${dumpImportFromProdGenerateServerProdDumpArchiveScriptDirectory}/../../../.utils/helpers/checkVariableIsDefined.sh) prodUser
 source $(realpath ${dumpImportFromProdGenerateServerProdDumpArchiveScriptDirectory}/../../../.utils/helpers/checkVariableIsDefined.sh) serverHost
 source $(realpath ${dumpImportFromProdGenerateServerProdDumpArchiveScriptDirectory}/../../../.utils/helpers/checkVariableIsDefined.sh) productionProjectPath
-source $(realpath ${dumpImportFromProdGenerateServerProdDumpArchiveScriptDirectory}/../../../.utils/helpers/checkVariableIsDefined.sh) productionDumpStoragePath
+source $(realpath ${dumpImportFromProdGenerateServerProdDumpArchiveScriptDirectory}/../../../.utils/helpers/checkVariableIsDefined.sh) productionDumpStorageDirectory
 source $(realpath ${dumpImportFromProdGenerateServerProdDumpArchiveScriptDirectory}/../../../.utils/helpers/checkVariableIsDefined.sh) productionDumpArchivePath
+
+echo -e "${gray}=================================================${reset}\n"
+
+# we create the production dump storage directory
+echo "${purple}▶${reset} Creating server ${productionDumpStorageDirectory} directory ..."
+echo "${purple}→ mkdir -p ${productionDumpStorageDirectory}${reset}"
+mkdir -p ${productionDumpStorageDirectory}
+echo -e "${green}✔${reset} Server ${productionDumpStorageDirectory} directory available.\n"
 
 echo -e "${gray}=================================================${reset}\n"
 
@@ -21,18 +29,18 @@ echo -e "${gray}=================================================${reset}\n"
 source $(realpath ${dumpImportFromProdGenerateServerProdDumpArchiveScriptDirectory}/../../../.utils/helpers/exportEnvFileVariables.sh)
 
 # we execute a production pgsql dump
-source $(realpath ${dumpImportFromProdGenerateServerProdDumpArchiveScriptDirectory}/../../../.utils/database/generatePgsqlDump.sh) ${productionDumpStoragePath}/nsn_dump.sql
+source $(realpath ${dumpImportFromProdGenerateServerProdDumpArchiveScriptDirectory}/../../../.utils/database/generatePgsqlDump.sh) ${productionDumpStorageDirectory}/nsn_dump.sql
 
 echo -e "${gray}=================================================${reset}\n"
 
 # we sync the production public/files directory with the dump files directory
 echo "${purple}▶${reset} Syncing the production public/files directory with the dump files directory ..."
-echo "${purple}→ rsync ${productionProjectPath}/shared/public/files/ ${productionDumpStoragePath}/files${reset}"
-rsync ${productionProjectPath}/shared/public/files/ ${productionDumpStoragePath}/files
+echo "${purple}→ rsync ${productionProjectPath}/shared/public/files/ ${productionDumpStorageDirectory}/files${reset}"
+rsync ${productionProjectPath}/shared/public/files/ ${productionDumpStorageDirectory}/files
 echo -e "${green}✔${reset} Production public/files directory synced with the dump files directory.\n"
 
 # we generate an archive from the production dump folder
 echo "${purple}▶${reset} Syncing the production public/files directory with the dump files directory ..."
-echo "${purple}→ rsync ${productionProjectPath}/shared/public/files/ ${productionDumpStoragePath}/files${reset}"
-tar czvf ${productionDumpArchivePath} ${productionDumpStoragePath} 
+echo "${purple}→ rsync ${productionProjectPath}/shared/public/files/ ${productionDumpStorageDirectory}/files${reset}"
+tar czvf ${productionDumpArchivePath} ${productionDumpStorageDirectory} 
 echo -e "${green}✔${reset} Production public/files directory synced with the dump files directory.\n"
--- .utils.custom/dump/prodToPreprod/additionalInstructions.sh
+++ .utils.custom/dump/prodToPreprod/additionalInstructions.sh
@@ -2,12 +2,10 @@
 
 # example
 
-# we generate a public/files archive and store it the /tmp/prod/delegations_dump directory
-echo "${purple}▶${reset} Copying the production public/files directory to the preprod project ..."
-echo "${purple}→ rm -rf ${preprodProjectPath}/shared/public/files${reset}"
-rm -rf ${preprodProjectPath}/shared/public/files
+# we sync the production files directory with the preprod project
+echo "${purple}▶${reset} Syncing the production public/files directory with the preprod project ..."
 echo "${purple}→ rsync ${productionProjectPath}/shared/public/files/ ${preprodProjectPath}/shared/public/files${reset}"
 rsync ${productionProjectPath}/shared/public/files/ ${preprodProjectPath}/shared/public/files
 echo "${purple}→ chown -R ${preprodUser}:users ${preprodProjectPath}/shared/public/files${reset}"
 chown -R ${preprodUser}:users ${preprodProjectPath}/shared/public/files
-echo -e "${green}✔${reset} files directory copied with success.\n"
+echo -e "${green}✔${reset} Files directory sync done.\n"
