7b2bf0eaf570c52e0c13b899bb7f5679530e79ba
Artu1000 <arthur.lorent@gmail.com>
2018-04-09 22:53:35 +0200

    uncommented custom script examples

--- .utils.custom/project/install.sh
+++ .utils.custom/project/install.sh
@@ -2,42 +2,42 @@
 
 # example
 
-#echo -e "${gray}=================================================${reset}\n"
-#
-## bower install / update
-#echo "${purple}▶${reset} Installing / updating bower dependencies ..."
-#if [ -d bower_components ]; then
-#    echo "${gray}Bower vendor directory detected${reset}"
-#    echo "${purple}→ ./node_modules/bower/bin/bower update${reset}"
-#    ./node_modules/bower/bin/bower update
-#    echo -e "${green}✔${reset} Bower dependencies updated\n"
-#else
-#    echo "${gray}Bower vendor directory NOT detected${reset}"
-#    echo "${purple}→ ./node_modules/bower/bin/bower install${reset}"
-#    ./node_modules/bower/bin/bower install
-#    echo -e "${green}✔${reset} Bower dependencies installed\n"
-#fi
-#
-#echo -e "${gray}=================================================${reset}\n"
-#
-## we prepare the storage folders
-#echo "${purple}▶${reset} Preparing storage folders ..."
-#echo "${purple}→ php artisan storage:prepare${reset}"
-#php artisan storage:prepare
-#echo "${green}✔${reset} Storage folders prepared"
-#
-#echo -e "${gray}=================================================${reset}\n"
-#
-## we prepare the symbolic links
-#echo "${purple}▶${reset} Preparing app symlinks ..."
-#echo "${purple}→ php artisan symlinks:prepare${reset}"
-#php artisan symlinks:prepare
-#echo "${green}✔${reset} App symlinks created"
-#
-#echo -e "${gray}=================================================${reset}\n"
-#
-## project optimizations
-#echo "${purple}▶${reset} Executing project optimizations ..."
-#echo "${purple}→ php artisan project:optimize${reset}"
-#php artisan project:optimize
-#echo -e "${green}✔${reset} Project optimizations done\n"
+echo -e "${gray}=================================================${reset}\n"
+
+# bower install / update
+echo "${purple}▶${reset} Installing / updating bower dependencies ..."
+if [ -d bower_components ]; then
+    echo "${gray}Bower vendor directory detected${reset}"
+    echo "${purple}→ ./node_modules/bower/bin/bower update${reset}"
+    ./node_modules/bower/bin/bower update
+    echo -e "${green}✔${reset} Bower dependencies updated\n"
+else
+    echo "${gray}Bower vendor directory NOT detected${reset}"
+    echo "${purple}→ ./node_modules/bower/bin/bower install${reset}"
+    ./node_modules/bower/bin/bower install
+    echo -e "${green}✔${reset} Bower dependencies installed\n"
+fi
+
+echo -e "${gray}=================================================${reset}\n"
+
+# we prepare the storage folders
+echo "${purple}▶${reset} Preparing storage folders ..."
+echo "${purple}→ php artisan storage:prepare${reset}"
+php artisan storage:prepare
+echo "${green}✔${reset} Storage folders prepared"
+
+echo -e "${gray}=================================================${reset}\n"
+
+# we prepare the symbolic links
+echo "${purple}▶${reset} Preparing app symlinks ..."
+echo "${purple}→ php artisan symlinks:prepare${reset}"
+php artisan symlinks:prepare
+echo "${green}✔${reset} App symlinks created"
+
+echo -e "${gray}=================================================${reset}\n"
+
+# project optimizations
+echo "${purple}▶${reset} Executing project optimizations ..."
+echo "${purple}→ php artisan project:optimize${reset}"
+php artisan project:optimize
+echo -e "${green}✔${reset} Project optimizations done\n"
--- .utils.custom/server/localesInstall.sh
+++ .utils.custom/server/localesInstall.sh
@@ -2,8 +2,8 @@
 
 # example
 
-## set as much locales as you need here
-#echo "${purple}→ locale-gen fr_FR.UTF-8${reset}"
-#locale-gen fr_FR.UTF-8
-#echo "${purple}→ locale-gen en_GB.UTF-8${reset}"
-#locale-gen en_GB.UTF-8
+# set as much locales as you need here
+echo "${purple}→ locale-gen fr_FR.UTF-8${reset}"
+locale-gen fr_FR.UTF-8
+echo "${purple}→ locale-gen en_GB.UTF-8${reset}"
+locale-gen en_GB.UTF-8
