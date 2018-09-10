19b380db76aea613391e9ec536bf7c9d3cabbc79
Arthur LORENT <Okipa@users.noreply.github.com>
2018-09-10 18:26:19 +0200

    Update setRequiredVariables.sh

--- .utils.custom/dump/prodToPreprod/setRequiredVariables.sh
+++ .utils.custom/dump/prodToPreprod/setRequiredVariables.sh
@@ -3,7 +3,9 @@
 # example
 
 serverPreprodUser="[server_preprod_user]"
+serverPreprodGroup="[server_preprod_group]"
 serverProdUser="[server_prod_user]"
+serverProdGroup="[server_prod_group]"
 serverPreprodProjectPath="/path/to/preprod/project/directory"
 serverProductionProjectPath="/path/to/production/project/directory"
 serverProductionSqlDumpStoragePath="/tmp/dumpImportFromProd"
