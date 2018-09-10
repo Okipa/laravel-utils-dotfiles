41df4ab272918704b1d41a812865f8d1986f59fb
Arthur LORENT <Okipa@users.noreply.github.com>
2018-09-10 18:34:17 +0200

    Update generateProductionSqlDump.sh

--- .utils.custom/dump/prodToPreprod/generateProductionSqlDump.sh
+++ .utils.custom/dump/prodToPreprod/generateProductionSqlDump.sh
@@ -6,4 +6,4 @@
 dumpProdToPreprodGenerateSqlDumpScriptDirectory=$(dirname "$(readlink -f ${BASH_SOURCE[0]})")
 
 # we execute a production pgsql dump
-source $(realpath ${dumpProdToPreprodGenerateSqlDumpScriptDirectory}/../../../.utils/database/generatePgsqlDump.sh) ${serverProductionSqlDumpStorageDirectory}/nsn_dump.sql
+source $(realpath ${dumpProdToPreprodGenerateSqlDumpScriptDirectory}/../../../.utils/database/generateMysqlDump.sh) ${serverProductionSqlDumpStorageDirectory}/dump.sql
