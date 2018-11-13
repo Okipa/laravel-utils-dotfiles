d61e917949b88d8499104e8385c6299d8eab60fd
Arthur LORENT <arthur@acid.fr>
2018-11-13 15:51:44 +0100

    updated utils scripts

--- docker/workspace.sh
+++ docker/workspace.sh
@@ -1,44 +1,43 @@
 #!/bin/bash
 
-# we get the current script directory
 dockerWorkspaceScriptDirectory=$(dirname "$(readlink -f ${BASH_SOURCE[0]})")
 
-# we export the .env file variables
 source $(realpath ${dockerWorkspaceScriptDirectory}/../helpers/exportEnvFileVariables.sh) --
 
-# we check that the variables required by the script are defined
 source $(realpath ${dockerWorkspaceScriptDirectory}/../helpers/checkVariableIsDefined.sh) PROJECT_PATH
 source $(realpath ${dockerWorkspaceScriptDirectory}/../helpers/checkVariableIsDefined.sh) DOCKER_DIRECTORY_PATH
 
-# we set the script variables
 arguments=$@
 
-# we set the script required variables
-setRequiredVariablesScript=${dockerWorkspaceScriptDirectory}/../../.utils.custom/docker/workspace/setRequiredVariables.sh
-if [ -f "${setRequiredVariablesScript}" ]; then
-    echo -e "${green}✔${reset} ${gray}The .utils.custom/docker/workspace/setRequiredVariables.sh custom instructions script has been detected and executed.${reset}\n"
-    source ${setRequiredVariablesScript}
-else
-    echo -e "${red}✗${reset} ${gray}No .utils.custom/docker/workspace/setRequiredVariables.sh script detected.${reset}\n"
-fi
+setRequiredVariablesScriptPath=${dockerWorkspaceScriptDirectory}/../../.utils.custom/docker/workspace/setRequiredVariables.sh
+source $(realpath ${dockerWorkspaceScriptDirectory}/../helpers/checkFileExists.sh) ${setRequiredVariablesScriptPath}
+source ${setRequiredVariablesScriptPath}
 source $(realpath ${dockerWorkspaceScriptDirectory}/../helpers/checkVariableIsDefined.sh) dockerUser
 
-# arguments
 if [[ ${arguments} = *'root'* ]]; then
     dockerUser='root'
 fi
 
-# we set the script functions
 function accessToWorkspaceSsh () {
     echo -e "${gray}=================================================${reset}\n"
 
-    echo "${purple}▶${reset} Accessing to docker workspace container ..."
+    echo "${purple}▶${reset} Moving to ${APP_NAME} docker directory ..."
     echo "${purple}→ cd ${DOCKER_DIRECTORY_PATH}${reset}"
     cd ${DOCKER_DIRECTORY_PATH}
+    echo -e "${green}✔${reset} Moved to ${APP_NAME} ${DOCKER_DIRECTORY_PATH} directory.\n"
+    
+    echo -e "${gray}=================================================${reset}\n"
+    
+    echo "${purple}▶${reset} Accessing to the ${APP_NAME} workspace container with the ${dockerUser} user ..."
     echo "${purple}→ docker-compose exec --user=${dockerUser} workspace bash${reset}"
     docker-compose exec --user=${dockerUser} workspace bash
-    echo "${purple}→ cd ${PROJECT_PATH}${reset}"
+    echo -e "${green}✔${reset} Accessed to the ${APP_NAME} workspace container.\n"
+    
+    echo -e "${gray}=================================================${reset}\n"
+    
+    echo "${purple}▶${reset} Moving back to ${APP_NAME} project directory ..."
     cd ${PROJECT_PATH}
+    echo -e "${green}✔${reset} Moved back to ${APP_NAME} project directory.\n"
 }
 
 # we execute the script treatments
